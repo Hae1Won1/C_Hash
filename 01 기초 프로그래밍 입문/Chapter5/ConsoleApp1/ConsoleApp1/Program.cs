bff23000ff203a7016dfd77ddff13c5164ba4eae
Hae1Won1 <h121581@naver.com>
2025-09-03 13:34:36 +0900

    Chapter6 TextRPG2

---
+++ 01 기초 프로그래밍 입문/Chapter5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class Knight
+        {
+            public int hp;
+            public int attack;
+
+            // 복사 
+            public Knight Clone()
+            {
+                Knight knight = new Knight();
+                knight.hp = hp;
+                knight.attack = attack;
+                return knight;
+            }
+
+
+
+            public void Move()
+            {
+                Console.WriteLine("Knight Move");
+            }
+            public void Attack()
+            {
+                Console.WriteLine("Knight Attack");
+            }
+        }
+
+        struct Mage
+        {
+            public int hp;
+            public int attack;
+        }
+
+        static void KillKight(Knight knight)
+        {
+            knight.hp = 0;
+        }
+
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 10;
+            KillKight(knight);
+            Console.WriteLine(knight.hp);
+
+            Mage mage = new Mage();
+            mage.hp = 100;
+            mage.attack = 10;
+            KillMage(mage);
+            Console.WriteLine(mage.hp);
+
+            Knight knight2 = knight.Clone();
+        }
+    }
+}
---
+++ 01 기초 프로그래밍 입문/Chapter6/TextRPG2/CSharp/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+    class Player : Creature
+    {
+        // _ 키워드는 이름 규정으로, 멤버변수라는 뜻/개인 호불호긴함
+        protected PlayerType _type;
+
+        // 외부에서 함부로 Player를 생성할 수 없도록 선언
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this._type = type;
+        }
+        public PlayerType GetPlayerTyoe() { return _type;  }
+ 
+    }
+
+    class Knight : Player
+    {
+        // 부모의 생성자 호출
+        public Knight() : base(PlayerType.Knight)
+        {
+            this.SetInfo(100, 10);
+        }
+
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        { 
+            this.SetInfo(75, 13);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            this.SetInfo(50, 15);
+        }
+    }
+}
